6980ad4f897f071b508082be207e0f6d156ca6e9
Tamar <tamar.behar@campus.technion.ac.il>
2021-06-25 15:20:29 +0300

    add BatchNormalization

--- eye_tracker_model.py
+++ eye_tracker_model.py
@@ -1,5 +1,6 @@
 import os
 from argparse import Namespace
+from datetime import datetime
 from typing import Dict, Tuple
 
 import tensorflow as tf
@@ -7,12 +8,13 @@ from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, MaxPool2D, Co
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Layer
 from tensorflow.python.keras.engine.keras_tensor import KerasTensor
+from tensorflow.keras.layers import BatchNormalization
 
 from EyeTrackongProject.eye_tracker_data import EyeTrackerData
 from EyeTrackongProject.eye_tracking_features import EyeTrackingFeatures
 import numpy as np
 from tensorflow.keras.optimizers import SGD, Adam
-from tensorflow.keras.callbacks import  EarlyStopping, ModelCheckpoint, TensorBoard
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 
 '''
 tensorflow (keras) model for the eye_tracker.
@@ -48,26 +50,42 @@ class EyeTrackerModel(tf.keras.Model):
     # Used for both eyes (with shared weights) and the face (with unqiue weights)
     def __init__(self, img_ch, img_cols, img_rows):
         super(EyeTrackerModel, self).__init__()
-        self._l_eye_input = Input(shape=(img_rows,img_cols, img_ch), name=EyeTrackingFeatures.LEFT_EYE_FRAME.value) # Input layout is BHWC
-        self._r_eye_input = Input(shape=(img_rows, img_cols, img_ch), name=EyeTrackingFeatures.RIGHT_EYE_FRAME.value)  # Input layout is BHWC
-        self._face_input = Input(shape=(img_rows, img_cols, img_ch), name=EyeTrackingFeatures.FACE_FRAME.value)  # Input layout is BHWC
-        self._face_grid_input = Input(shape=(625,), name=EyeTrackingFeatures.FACE_GRID.value)  # Input layout is BHWC # TODO: get from args
+        self._l_eye_input = Input(shape=(img_rows, img_cols, img_ch),
+                                  name=EyeTrackingFeatures.LEFT_EYE_FRAME.value)  # Input layout is BHWC
+        self._r_eye_input = Input(shape=(img_rows, img_cols, img_ch),
+                                  name=EyeTrackingFeatures.RIGHT_EYE_FRAME.value)  # Input layout is BHWC
+        self._face_input = Input(shape=(img_rows, img_cols, img_ch),
+                                 name=EyeTrackingFeatures.FACE_FRAME.value)  # Input layout is BHWC
+        self._face_grid_input = Input(shape=(625,),
+                                      name=EyeTrackingFeatures.FACE_GRID.value)  # Input layout is BHWC # TODO: get from args
         # TODO: add l2 regularization - READ about L2 regularization
         self._eye_model = tf.keras.Sequential(
             [Conv2D(96, (11, 11), activation=EyeTrackingFeatures.RELU.value),
+             BatchNormalization(),
              MaxPool2D(pool_size=(2, 2)),
+             BatchNormalization(),
              Conv2D(256, (5, 5), activation=EyeTrackingFeatures.RELU.value),
+             BatchNormalization(),
              MaxPool2D(pool_size=(2, 2)),
+             BatchNormalization(),
              Conv2D(384, (3, 3), activation=EyeTrackingFeatures.RELU.value),
-             MaxPool2D(pool_size=(2, 2))])
+             BatchNormalization(),
+             MaxPool2D(pool_size=(2, 2)),
+             BatchNormalization()])
 
         self._face_model = tf.keras.Sequential(
             [Conv2D(96, (11, 11), activation=EyeTrackingFeatures.RELU.value),
+             BatchNormalization(),
              MaxPool2D(pool_size=(2, 2)),
+             BatchNormalization(),
              Conv2D(256, (5, 5), activation=EyeTrackingFeatures.RELU.value),
+             BatchNormalization(),
              MaxPool2D(pool_size=(2, 2)),
+             BatchNormalization(),
              Conv2D(384, (3, 3), activation=EyeTrackingFeatures.RELU.value),
-             MaxPool2D(pool_size=(2, 2))])
+             BatchNormalization(),
+             MaxPool2D(pool_size=(2, 2)),
+             BatchNormalization(), ])
 
     def get_eye_tracker_model(self) -> tf.keras.models:
         # right eye model
@@ -85,21 +103,36 @@ class EyeTrackerModel(tf.keras.Model):
         # dense layers for eyes
         eye_features = Concatenate()([left_eye_features, right_eye_features])
         eye_features = Flatten()(eye_features)
-        fc_e1 = Dense(128, activation=EyeTrackingFeatures.RELU.value)(eye_features)
+        eye_features = BatchNormalization()(eye_features)
+        fc_e1 = Dense(128, activation=EyeTrackingFeatures.RELU.value, kernel_regularizer='l2')(eye_features)
+        fc_e1 = BatchNormalization()(fc_e1)
 
         # dense layers for face
         face_features = Flatten()(face_features)
-        fc_f1 = Dense(128, activation=EyeTrackingFeatures.RELU.value)(face_features)
-        fc_f2 = Dense(64, activation=EyeTrackingFeatures.RELU.value)(fc_f1)
+        face_features = BatchNormalization()(face_features)
+        fc_f1 = Dense(128, activation=EyeTrackingFeatures.RELU.value,
+                      kernel_regularizer=tf.keras.regularizers.l2(l=0.1))(face_features)
+        fc_f1 = BatchNormalization()(fc_f1)
+        fc_f2 = Dense(64, activation=EyeTrackingFeatures.RELU.value,
+                      kernel_regularizer=tf.keras.regularizers.l2(l=0.1))(fc_f1)
+        fc_f2 = BatchNormalization()(fc_f2)
 
         # dense layers for face grid
-        fc_fg1 = Dense(256, activation=EyeTrackingFeatures.RELU.value)(self._face_grid_input)
-        fc_fg2 = Dense(128, activation=EyeTrackingFeatures.RELU.value)(fc_fg1)
+        fc_fg1 = Dense(256, activation=EyeTrackingFeatures.RELU.value,
+                       kernel_regularizer=tf.keras.regularizers.l2(l=0.1))(self._face_grid_input)
+        fc_fg1 = BatchNormalization()(fc_fg1)
+        fc_fg2 = Dense(128, activation=EyeTrackingFeatures.RELU.value,
+                       kernel_regularizer=tf.keras.regularizers.l2(l=0.1))(fc_fg1)
+        fc_fg2 = BatchNormalization()(fc_fg2)
 
         # final dense layers
         h = Concatenate()([fc_e1, fc_f2, fc_fg2])
-        fc1 = Dense(128, activation=EyeTrackingFeatures.RELU.value)(h)
-        fc2 = Dense(2, activation=EyeTrackingFeatures.LINEAR.value)(fc1)
+        fc1 = Dense(128, activation=EyeTrackingFeatures.RELU.value, kernel_regularizer=tf.keras.regularizers.l2(l=0.1))(
+            h)
+        fc1 = BatchNormalization()(fc1)
+        fc2 = Dense(2, activation=EyeTrackingFeatures.LINEAR.value, kernel_regularizer=tf.keras.regularizers.l2(l=0.1))(
+            fc1)
+        fc2 = BatchNormalization()(fc2)
 
         # final model
         final_model = Model(
@@ -108,30 +141,23 @@ class EyeTrackerModel(tf.keras.Model):
 
         return final_model
 
-    def _prepare_data_for_model(self, model:tf.keras.Model, data: tf.data.Dataset, split: str, args: Namespace):
+    def _prepare_data_for_model(self, model: tf.keras.Model, data: tf.data.Dataset, split: str, args: Namespace):
         def _map_method(example: Dict[str, KerasTensor]) -> Tuple[Tuple[KerasTensor], Tuple[KerasTensor]]:
             in_features = tuple(example[name] for name in model.input_names)
-            out_features = tf.stack([example[EyeTrackingFeatures.LABEL_DOT_X_CAM.value], example[EyeTrackingFeatures.LABEL_DOT_Y_CAM.value]], axis=0)
+            out_features = tf.stack([example[EyeTrackingFeatures.LABEL_DOT_X_CAM.value],
+                                     example[EyeTrackingFeatures.LABEL_DOT_Y_CAM.value]], axis=0)
             return (in_features, out_features)
 
         data = data.map(_map_method)
         if split == "train":
-            data = data.shuffle(buffer_size=100)# TODO: increase
+            data = data.shuffle(buffer_size=100)  # TODO: increase
         data = data.batch(batch_size=args.batch_size)
         data = data.prefetch(100)
         return data
 
     def train_model(self, args):
-
         # train parameters
-        n_epoch = args.max_epoch
-        batch_size = args.batch_size
         patience = args.patience
-
-        # image parameter
-        img_cols = 64
-        img_rows = 64
-        img_ch = 3
 
         # model
         model = self.get_eye_tracker_model()
@@ -152,93 +178,30 @@ class EyeTrackerModel(tf.keras.Model):
         # compile model
         model.compile(optimizer=adam, loss='mse')
 
-        # data
-        # todo: parameters not hardocoded
-
-        # debug
-        # x, y = load_batch([l[0:batch_size] for l in train_data], img_ch, img_cols, img_rows)
-        # x, y = load_batch_from_names(train_names[0:batch_size], dataset_path, img_ch, img_cols, img_rows)
         eye_tracker_data = EyeTrackerData(path_to_prepared_data=r'C:\Users\Tamar\Desktop\hw\project\prepared_data')
 
-        train_data = self._prepare_data_for_model(model=model, data=eye_tracker_data.preprocess_data(set_mode='train'), split='train', args=args)
-        val_data = self._prepare_data_for_model(model=model, data=eye_tracker_data.preprocess_data(set_mode='test'), split='test', args=args)
+        train_data = self._prepare_data_for_model(model=model, data=eye_tracker_data.preprocess_data(set_mode='train'),
+                                                  split='train', args=args)
+        val_data = self._prepare_data_for_model(model=model, data=eye_tracker_data.preprocess_data(set_mode='val'),
+                                                split='val', args=args)
+
+        time_stamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
         model.fit(train_data,
                   epochs=args.max_epoch,
                   callbacks=[EarlyStopping(patience=patience),
-                             ModelCheckpoint("weights_big/weights.{epoch:03d}-{val_loss:.5f}.hdf5", # todo: add timestamp
+                             ModelCheckpoint("weights_big/weights.{epoch:03d}-{val_loss:.5f}_" + time_stamp + ".hdf5",
                                              save_best_only=True,
                                              verbose=1),
-                             TensorBoard("weights_big/logs", update_freq=10) # TODO: add timestamp
+                             TensorBoard(f"weights_big/logs_{time_stamp}", update_freq=10)
                              ],
                   validation_data=val_data)
 
-    # def test_model(self, args):
-    #
-    #     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-    #     os.environ["CUDA_VISIBLE_DEVICES"] = args.dev
-    #
-    #     names_path = ""
-    #     print("Names to test: {}".format(names_path))
-    #
-    #     dataset_path = ""
-    #     print("Dataset: {}".format(names_path))
-    #
-    #     weights_path = ""
-    #     print("Weights: {}".format(weights_path))
-    #
-    #     # image parameter
-    #     img_cols = 64
-    #     img_rows = 64
-    #     img_ch = 3
-    #
-    #     # test parameter
-    #     batch_size = 64
-    #     chunk_size = 500
-    #
-    #     # model
-    #     model = self._get_eye_tracker_model(img_ch, img_cols, img_rows)
-    #
-    #     # model summary
-    #     model.summary()
-    #
-    #     # weights
-    #     print("Loading weights...")
-    #     model.load_weights(weights_path)
-    #
-    #     # data
-    #     test_names = load_data_names(names_path)
-    #
-    #     # limit amount of testing data
-    #     # test_names = test_names[:1000]
-    #
-    #     # results
-    #     err_x = []
-    #     err_y = []
-    #
-    #     print("Loading testing data...")
-    #     for it in list(range(0, len(test_names), chunk_size)):
-    #
-    #         x, y = load_batch_from_names_fixed(test_names[it:it + chunk_size],  dataset_path, img_ch, img_cols, img_rows)
-    #         # x, y = load_batch_from_names(test_names[it:it + chunk_size], dataset_path, img_ch, img_cols, img_rows)
-    #         predictions = model.predict(x=x, batch_size=batch_size, verbose=1)
-    #
-    #         # print and analyze predictions
-    #         for i, prediction in enumerate(predictions):
-    #             print("PR: {} {}".format(prediction[0], prediction[1]))
-    #             print("GT: {} {} \n".format(y[i][0], y[i][1]))
-    #
-    #             err_x.append(abs(prediction[0] - y[i][0]))
-    #             err_y.append(abs(prediction[1] - y[i][1]))
-    #
-    #     # mean absolute error
-    #     mae_x = np.mean(err_x)
-    #     mae_y = np.mean(err_y)
-    #
-    #     # standard deviation
-    #     std_x = np.std(err_x)
-    #     std_y = np.std(err_y)
-    #
-    #     # final results
-    #     print("MAE: {} {} ( samples)".format(mae_x, mae_y))
-    #     print("STD: {} {} ( samples)".format(std_x, std_y))
+    def evaluate_model(self, path_to_model: str, args):
+        model_to_eval = tf.keras.models.load_model(path_to_model)
+        model_to_eval.summary()
+        eye_tracker_data = EyeTrackerData(path_to_prepared_data=r'C:\Users\Tamar\Desktop\hw\project\prepared_data')
+        test_data = self._prepare_data_for_model(model=model_to_eval, data=eye_tracker_data.preprocess_data(set_mode='test'),
+                                                  split='test', args=args)
+        loss = model_to_eval.evaluate(test_data, verbose=2)
+        print(f"loss:{loss}")
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from eye_tracker_model import EyeTrackerModel
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-train', action='store_true', help='train flag')
-    parser.add_argument('-test', action='store_true', help='train flag')
+    parser.add_argument('-eval', action='store_true', help='evaluate flag')
     parser.add_argument('-data', type=str, default='small', help='which dataset, small or big')
     parser.add_argument('-max_epoch', '--max_epoch', type=int, default=100, help='max number of iterations (default 100)')
     parser.add_argument('-batch_size', type=int, default=2, help='batch size (default 50)')
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     if args.train:
         eye_tracker_model.train_model(args)
 
-    # test
-    if args.test:
-        eye_tracker_model.test_model(args)
-
+    # eval
+    if args.eval:
+        eye_tracker_model.evaluate_model(args=args,
+                                         path_to_model=r'C:\Users\Tamar\PycharmProjects\EyeTrackingProject\EyeTrackongProject\weights_big\weights.001-245.97839.hdf5')
